ca55583c1908cf11c09a317c824e584f81f61897
Daniel B <laznin@gmail.com>
2017-10-25 04:30:37 +0000

    second commit

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-var http = require('http');
+'use strict';
 var fs = require('fs');
 
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', null, function(error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-}
-
-http.createServer(onRequest).listen(80);
+exports.get = function(event, context) {
+  var contents = fs.readFileSync("./index.html");
+  context.succeed({
+    statusCode: 200,
+    body: contents.toString(),
+    headers: {'Content-Type': 'text/html'}
+  });
+};
